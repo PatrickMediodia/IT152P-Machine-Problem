6e59bd00e724fd512ff5fa4df79b8359aa496cb9
Pat <50007415+PatrickMediodia@users.noreply.github.com>
2022-03-05 21:40:37 +0800

    Add files via upload

--- Home.php
+++ Home.php
@@ -19,7 +19,7 @@
     <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js" type="text/javascript" charset="utf8"></script>
     <script src="https://cdn.datatables.net/rowreorder/1.2.7/js/dataTables.rowReorder.min.js" type="text/javascript" charset="utf8"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.7/js/dataTables.responsive.min.js" type="text/javascript" charset="utf8"></script>
-
+    
     <script src="script.js"></script>
     <link href="style.css" rel="stylesheet">
 </head>
@@ -27,61 +27,76 @@
 <body background="https://wallpaperaccess.com/full/1092564.jpg">
     <div class="container-fluid text-center col-xl-10 col-lg-9 col-md-10 col-sm-11" style="padding-bottom:30px; padding-top:30px;">
         <div class="rcorners">
-            <h1 style="padding:50px; color:#1055A1;">Temperature and Humidity Records</h1>
+            <h1 style="padding:50px; color:#2E6CA4;"><strong>Temperature and Humidity Records</strong></h1>
             <div class="row" style="padding:30px;">
-                <div class="col-lg-4">
-                    <div class="row">
-                        <p><span class="smallHeading">Average Temperature:<br /></span>
-                            <span class="number" id="averageTemp"></span>
-                        </p>
-                    </div>
-                    <div class="row">
-                        <p><span class="smallHeading">Average Humidity:<br /></span>
-                            <span class="number" id="averageHum"></span>
-                        </p>
-                    </div>
-                    <div class="row">
-                        <p><span class="smallHeading">Desired Temp:<br /></span>
-                            <span class="number" id="desiredTemp"></span>
-                        </p>
-                    </div>
-                    <div class="row">
-                        <p><span class="smallHeading">Current Temp:<br /></span>
-                            <span class="number" id="currentTemp"></span>
-                        </p>
-                    </div>
-                    <div class="row">
-                        <form action="WebService/updateDesiredTemp.php" method="POST">
-                            <input type="text" class="form-control" name="desiredTemp">
-                            <input type="submit" name="submit" value="Submit">
-                        </form>
-                    </div>
-                    <!--
-                    <div class="row">
-                        <form action="Email/updateEmail.php" method="POST">
-                            <input type="text" name="updateEmail">
-                            <input type="submit" name="submit" value="Submit">
-                        </form>
-                    </div>
-                    -->
+                <!-- Temp/Hum Summary -->
+                <div class="col-lg-3">
+                    <p><span class="smallHeading">Average Temperature:<br /></span>
+                        <span class="number" id="averageTemp"></span>
+                    </p>
                 </div>
-                <div class="col-lg-8">
-                    <table id="example" class="display tabletable-bordered" style="width:100%">
+                <div class="col-lg-3">
+                    <p><span class="smallHeading">Average Humidity:<br /></span>
+                        <span class="number" id="averageHum"></span>
+                    </p>
+                </div>
+                <div class="col-lg-3">
+                    <p><span class="smallHeading">Desired Temperature:<br /></span>
+                        <span class="number" id="desiredTemp"></span>
+                    </p>
+                </div>
+                <div class="col-lg-3">
+                    <p><span class="smallHeading">Current Temperature:<br /></span>
+                        <span class="number" id="currentTemp"></span>
+                    </p>
+                </div>
+            </div> <!-- end of Temp/Hum Summary row -->
+
+            <div class="row" style="padding:40px 0px; width:80%; margin:auto;">
+                <!-- Fan/Device Status, Update Desired Row -->
+                <div class="col-md-4" style="padding-top:40px;">
+                    <img src="Images/BlueFan.png" alt="Fan Icon" height="230" width="230">
+                </div>
+                <div class="col-md-8" style="padding-top:30px;">
+                    <div class="row">
+                        <div class="col-lg-6" style="text-align:center;">
+                            <span class="smallHeading">Fan Status: </span><br>
+                            <span class="number" id="fanStatus"></span>
+                        </div>
+                        <div class="col-lg-6">
+                            <span class="smallHeading">Device Status: </span><br>
+                            <span class="number" id="deviceStatus"></span>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <span class="smallHeading">Update Desired Temperature:<br /></span> <!-- Update Desired -->
+                    <form action="WebService/updateDesiredTemp.php" method="POST">
+                        <div class="row" style="padding-top:10px">
+                            <div style="width:60%;"><input type="text" placeholder="New Temperature" name="desiredTemp" value=""></div>
+                            <div style="width:40%;"><input class="button2" type="submit" name="submit" value="UPDATE"></div>
+                        </div>
+                    </form>
+                </div>
+            </div> <!--  End of Fan/Device Status, Update Desired Row -->
+
+            <hr>
+
+            <h1 style="padding:20px; color:#2E6CA4;"><strong>Logs</strong></h1>
+            <div class="d-flex justify-content-center">
+                <div class="col-lg-11">
+                    <table id="example" class="display table table-bordered" style="width: 100%;">
                         <thead>
                             <tr>
-                                <th>Temperature</th>
+                                <th>id</th>
+                                <th>Current Temperature</th>
+                                <th>Desired Temperature</th>
                                 <th>Humidity</th>
                                 <th>Date and Time</th>
                             </tr>
                         </thead>
                     </table>
-                </div>
-            </div>
-            <div>
-                <div class="row">
-                    <p><span class="smallHeading">Fan Status<br /></span>
-                        <span class="number" id="fanStatus"></span>
-                    </p>
                 </div>
             </div>
         </div>
--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 $(document).ready(function() {
-    var table = $('#example').DataTable( {
-        "order": [[ 2, "desc" ]],
+    var table = $('#example').DataTable({
+
+        //add color if temp is too hot
+        "createdRow": function( row, data, dataIndex ) {
+            console.log(data[1], data[2]);
+            if (data[1] > data[2]) {        
+            $(row).css('background-color', 'rgba(128,0,0,0.8)');
+            $(row).css('color', 'rgba(255,255,255,0.8)');
+        }},
+
+        //order by id, descending
+        "order": [[ 0, "desc" ]],        
+
+        //hide id
+        "columnDefs": [{
+            "targets": [ 0 ],
+            "visible": false,
+            "searchable": false 
+        }],
+
+        //get data from web service
         ajax: "WebService/getTempHumRecords.php"
     });
-     
+    
+    //refresh table every second
     setInterval(function() {
         table.ajax.reload(null, false); // user paging is not reset on reload
     }, 1000);
 
     getAverageRecords();
     getDesiredTemp();
-    getCurrentTemp();
-    updateFanStatus();
+    updateDeviceStatus();
 
     setInterval(getAverageRecords, 1000);
     setInterval(getDesiredTemp, 1000);
-    setInterval(getCurrentTemp, 1000);
-    setInterval(updateFanStatus, 1000);
+    setInterval(updateDeviceStatus, 1000);
 });
 
 function getAverageRecords() {
@@ -57,6 +75,24 @@ function updateFanStatus() {
         url: 'WebService/getFanStatus.php',
         success: function(data) {
             $('#fanStatus').html(data);
+        }
+    });
+}
+
+function updateDeviceStatus() {
+    $.ajax({
+        type: 'POST',
+        url: 'WebService/getDeviceStatus.php',
+        success: function(data) {
+            $('#deviceStatus').html(data);
+            if(data == "OFFLINE") {
+                $('#fanStatus').html("OFF");
+                $('#currentTemp').html("N/A");
+            }
+            else {
+                updateFanStatus();
+                getCurrentTemp();
+            }
         }
     });
 }
