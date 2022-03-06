508ccfd3c650482ee05f71400c5a342313bdc22c
Pat <50007415+PatrickMediodia@users.noreply.github.com>
2022-03-06 22:06:17 +0800

    Delete getCurrentTemp.php

--- WebService/getCurrentTemp.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once('connects.php');
-
-	$query = "SELECT * FROM `temperature`";
-	$result = mysqli_query($con,$query);
-    $row = mysqli_fetch_assoc($result); 
-	
-	echo $row['currentTemp']; 
-?>
