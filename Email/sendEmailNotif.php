dc576ffdddd089ec4f629caf37dcd60fffeb9801
Ava Lacambra <acolacambra@gmail.com>
2022-03-06 23:10:03 +0800

    update email notification

--- Email/sendEmailNotif.php
+++ Email/sendEmailNotif.php
@@ -2,13 +2,32 @@
 
 include_once('connects.php');
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->SMTPAuth(true);
+$mail->SMTPSecure = 'ssl';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = '465';
+
+$mail->Username = 'japatalava@gmail.com';
+$mail->Password = 'japatalava1234';
+
 $query = "SELECT * FROM emailrecords";
 $result = mysqli_query($con,$query);
 
-$msg= "Temperature is higher than the desired temperature. Fan is triggered.";
-$msg=wordwrap($msg,70);
-
-while($row=mysqli_fetch_array($result)) {    
-    mail($row['email'],"Temperature Update",$msg);
+while($row=mysqli_fetch_array($result)) { 
+    $mail->From =('japatalava@gmail.com');
+    $mail->Subject = 'Temperature Update';
+    $mail->Body = 'Temperature is higher than desired temperaute. Fan is triggered';   
+    //mail($row['email'],"Temperature Update",$msg);
+    $mail->AddAddress($row['email']);
+    $mail->Send();
 }
 ?>
